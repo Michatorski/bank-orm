87e496c446191ff960d04bf96297786888891fa5
Michatorski <MichalHomend@protonmail.com>
2021-12-13 18:24:57 +0100

    haciendo un poco de Mikel

--- src/main/java/org/iesfm/bank/controllers/CustomerController.java
+++ src/main/java/org/iesfm/bank/controllers/CustomerController.java
@@ -1,4 +1,11 @@
 package org.iesfm.bank.controllers;
 
+import org.iesfm.bank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
 }
--- src/main/java/org/iesfm/bank/repository/CustomerRepository.java
+++ src/main/java/org/iesfm/bank/repository/CustomerRepository.java
@@ -1,4 +1,16 @@
 package org.iesfm.bank.repository;
 
-public interface CustomerRepository {
+import org.iesfm.bank.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> listCustomers();
+
+    boolean insert();
+
+    boolean delete();
+
+    List<Customer> listCustomers(String nif);
 }
