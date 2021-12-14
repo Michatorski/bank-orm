648d014692c8e1fa076aec1332132923ce8b0d01
Michatorski <MichalHomend@protonmail.com>
2021-12-14 18:33:56 +0100

    CustomerController and Repository are done

--- src/main/java/org/iesfm/bank/controllers/CustomerController.java
+++ src/main/java/org/iesfm/bank/controllers/CustomerController.java
@@ -1,11 +1,43 @@
 package org.iesfm.bank.controllers;
 
+import org.iesfm.bank.Customer;
 import org.iesfm.bank.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/customers")
+    public List<Customer> list() {
+    return customerRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/customers/{id}")
+    public Customer get(@PathVariable("id") int id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+
+        if (customerOptional.isEmpty()){
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encuentra  ale cluiente");
+        } else {
+            return  customerOptional.get();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/customers")
+    public void insert(@RequestBody Customer customer){
+        customerRepository.save(customer);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/customers/{id}")
+    public void delete(@PathVariable("id") int id){
+        customerRepository.deleteById(id);
+    }
 }
---
+++ src/main/java/org/iesfm/bank/repository/AccountRepository.java
@@ -0,0 +1,4 @@
+package org.iesfm.bank.repository;
+
+public interface AccountRepository {
+}
