15575ac48fec5cc3426a78894e02de26616147c2
Michatorski <MichalHomend@protonmail.com>
2021-12-16 00:55:43 +0100

    Main needs fixing!

--- src/main/java/org/iesfm/bank/Main.java
+++ src/main/java/org/iesfm/bank/Main.java
@@ -1,4 +1,10 @@
 package org.iesfm.bank;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class Main {
+  SpringApplication.run(Main.class, args);
+
+
 }
